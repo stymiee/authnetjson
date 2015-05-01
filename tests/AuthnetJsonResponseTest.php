4fc5a587bdb14ed4651b72283791e5663d62b56c
John Conde <john@johnconde.net>
2015-05-01 09:39:02 -0400

    Added test relation to Transaction Details

--- tests/AuthnetJsonResponseTest.php
+++ tests/AuthnetJsonResponseTest.php
@@ -56,8 +56,8 @@ class AuthnetJsonResponseTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($response->isSuccessful());
         $this->assertFalse($response->isError());
         $this->assertEquals('Ok', $response->messages->resultCode);
-        $this->assertEquals('I00004', $response->transactionResponse->messages[0]->code);
-        $this->assertEquals('No records found.', $response->transactionResponse->messages[0]->text);
+        $this->assertEquals('I00004', $response->messages->message[0]->code);
+        $this->assertEquals('No records found.', $response->messages->message[0]->text);
     }
 
     /**
@@ -83,7 +83,31 @@ class AuthnetJsonResponseTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($response->isSuccessful());
         $this->assertTrue($response->isError());
         $this->assertEquals('Error', $response->messages->resultCode);
-        $this->assertEquals('E00027', $response->transactionResponse->messages[0]->code);
-        $this->assertEquals('The transaction was unsuccessful.', $response->transactionResponse->messages[0]->text);
+        $this->assertEquals('E00027', $response->messages->message[0]->code);
+        $this->assertEquals('The transaction was unsuccessful.', $response->messages->message[0]->text);
+    }
+
+    /**
+     * @covers            \JohnConde\Authnet\AuthnetJsonResponse::getTransactionResponseField()
+     * @covers            \JohnConde\Authnet\TransactionResponse::getTransactionResponseField()
+     */
+    public function testTransactionResponse()
+    {
+        $responseJson = '{
+           "directResponse":"1,1,1,This transaction has been approved.,902R0T,Y,2230582306,INV000001,description of transaction,10.95,CC,auth_capture,12345,John,Smith,,123 Main Street,Townsville,NJ,12345,,800-555-1234,,user@example.com,John,Smith,,123 Main Street,Townsville,NJ,12345,,1.00,,2.00,FALSE,PONUM000001,D3B20D6194B0E86C03A18987300E781C,P,2,,,,,,,,,,,XXXX1111,Visa,,,,,,,,,,,,,,,,,29366174",
+           "messages":{
+              "resultCode":"Ok",
+              "message":[
+                 {
+                    "code":"I00001",
+                    "text":"Successful."
+                 }
+              ]
+           }
+        }';
+
+        $response = new AuthnetJsonResponse($responseJson);
+
+        $this->assertEquals('2230582306', $response->getTransactionResponseField('TransactionID'));
     }
 }
