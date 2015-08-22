222ccd8e214dc4ef6b5ac8d5f0b48169ef17551a
John Conde <john@johnconde.net>
2015-08-21 20:52:53 -0400

    Added an extra * to turn inital comment into file comment to make phpdocumentor happy.

--- src/autoload.php
+++ src/autoload.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the AuthnetJSON package.
+ *
+ * (c) John Conde <stymiee@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 spl_autoload_register(
     function($class) {
         static $classes = null;
