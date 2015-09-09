7e948d03b2e3fe52288f9222b7cdf2c0dc821b59
John Conde <john@johnconde.net>
2015-09-08 21:44:28 -0400

    Added for 2.5 release

---
+++ src/exceptions/AuthnetInvalidAmountException.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the AuthnetJSON package.
+ *
+ * (c) John Conde <stymiee@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JohnConde\Authnet;
+
+/**
+ * Exception that is thrown when invalid amount to pay is provided for a SIM transaction
+ *
+ * @package    AuthnetJSON
+ * @author     John Conde <stymiee@gmail.com>
+ * @copyright  John Conde <stymiee@gmail.com>
+ * @license    http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
+ * @link       https://github.com/stymiee/authnetjson
+ */
+class AuthnetInvalidAmountException Extends AuthnetException {}
