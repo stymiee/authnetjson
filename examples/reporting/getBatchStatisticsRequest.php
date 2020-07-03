e852e6eba61106eb65bff8fb21b294b76d738b32
John Conde <stymiee@gmail.com>
2020-07-02 22:41:15 -0400

    Improved example to show how more data is accessed

--- examples/reporting/getBatchStatisticsRequest.php
+++ examples/reporting/getBatchStatisticsRequest.php
@@ -129,6 +129,14 @@ try {
             <td><?= $response->isError() ? 'yes' : 'no' ?></td>
         </tr>
         <tr>
+            <th>Code</th>
+            <td><?= $response->messages->message[0]->code ?></td>
+        </tr>
+        <tr>
+            <th>Message</th>
+            <td><?= $response->messages->message[0]->text ?></td>
+        </tr>
+        <tr>
             <th>Batch</th>
             <td>
                 batchId: <?php echo $response->batch->batchId; ?><br>
