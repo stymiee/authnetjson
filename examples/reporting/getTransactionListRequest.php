b3a1dee9f1767e2ac4e0989cc89af35592513879
John Conde <stymiee@gmail.com>
2020-07-03 09:09:34 -0400

    Improved example to show how more data is accessed

--- examples/reporting/getTransactionListRequest.php
+++ examples/reporting/getTransactionListRequest.php
@@ -111,6 +111,14 @@ try {
             <th>Error?</th>
             <td><?= $response->isError() ? 'yes' : 'no' ?></td>
         </tr>
+        <tr>
+            <th>Code</th>
+            <td><?= $response->messages->message[0]->code ?></td>
+        </tr>
+        <tr>
+            <th>Message</th>
+            <td><?= $response->messages->message[0]->text ?></td>
+        </tr>
         <?php foreach ($response->transactions as $transaction) : ?>
         <tr>
             <th>Transaction</th>
